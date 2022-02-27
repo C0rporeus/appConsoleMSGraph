332bfd671e495fade5d5eee91d992a1bb170d4d3
Yonathan Gutierrez <jhony2473@gmail.com>
2022-02-27 18:15:43 -0500

    charge connect api and permissions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
+using Helpers;
+
+namespace graphconsoleapp
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var config = LoadAppSettings();
+      if (config == null)
+      {
+        Console.WriteLine("invalid appsettings.json file");
+        return;
+      }
+      var client = GetAuthenticatedGraphClient(config);
+
+      var graphRequest = client
+                         .Users.Request()
+                         .Select(u => new {u.Id, u.DisplayName, u.Mail });
+
+      var result = graphRequest.GetAsync().Result;
+      
+      foreach(var user in result)
+      {
+        Console.WriteLine(user.Id + ": " + user.DisplayName + " <" + user.Mail + ">");
+      }
+      Console.WriteLine("\nGraph Request:");
+      Console.WriteLine(graphRequest.GetHttpRequestMessage().RequestUri);
+    }
+    public static GraphServiceClient? _graphClient;
+    private static IConfigurationRoot? LoadAppSettings()
+    {
+      try
+      {
+        var config = new ConfigurationBuilder()
+          .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json", false, true)
+          .Build();
+        if(string.IsNullOrEmpty(config["applicationId"]) ||
+           string.IsNullOrEmpty(config["applicationSecret"]) ||
+           string.IsNullOrEmpty(config["redirectUri"]) ||
+           string.IsNullOrEmpty(config["tenantId"]))
+        {
+          return null;
+        } 
+        return config;
+      }
+      catch (System.IO.FileNotFoundException)
+      {
+        return null;
+      }
+    }
+    private static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config)
+    {
+      var clientId = config["applicationId"];
+      var clientSecret = config["applicationSecret"];
+      var redirectUri = config["redirectUri"];
+      var authority = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
+
+      List<string> scopes = new List<string>();
+      scopes.Add("https://graph.microsoft.com/.default");
+
+      var cca = ConfidentialClientApplicationBuilder.Create(clientId)
+        .WithAuthority(authority)
+        .WithRedirectUri(redirectUri)
+        .WithClientSecret(clientSecret)
+        .Build();
+      return new MsalAuthenticationProvider(cca, scopes.ToArray());
+    }
+    private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config)
+    {
+      var authenticationprovider = CreateAuthorizationProvider(config);
+      _graphClient = new GraphServiceClient(authenticationprovider);
+      return _graphClient;
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## App Console users for MS Graph
+
+This app using service from Tenant Azure AD for Office 365, getting data from users and Id's from interactive whit data from Azure AD.
+
+`dotnet build`
+
+from compiling app
+
+```dotnet run```
+
